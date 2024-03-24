da82252a9131acdb14becf95bd195046033fce3d
Agustín Stringa <stringaagustin1@gmail.com>
2024-03-24 11:40:25 -0300

    adding first labs

---
+++ Lab02/Lab02/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string valor1 = "Esta es la cadena 1";
+int valor2 = 234;
+string valor3 = valor1;
+Console.WriteLine("el valor 1 es " + valor1);
+Console.WriteLine("el valor 2 es " + valor2);
+Console.WriteLine("el valor 3 es " + valor3);
+Console.ReadKey();
